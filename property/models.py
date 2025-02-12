3e1d3f6437ac4f391ec9ebd783ed55332764da7e
Evgenii Bykovskii <bykovsky.ssau@gmail.com>
2025-02-13 00:16:27 +0400

    add related_name

--- property/models.py
+++ property/models.py
@@ -69,10 +69,12 @@ class Flat(models.Model):
 class Complaint(models.Model):
     user = models.ForeignKey(User,
                              on_delete=models.CASCADE,
-                             verbose_name='Кто жаловался')
+                             verbose_name='Кто жаловался',
+                             related_name='complaints')
     flat = models.ForeignKey(Flat,
                              on_delete=models.CASCADE,
-                             verbose_name='Квартира, на которую пожаловались')
+                             verbose_name='Квартира, на которую пожаловались',
+                             related_name='complaints')
     text = models.TextField('Текст жалобы')
 
     def __str__(self):
